a5d539329d1ed80f87874ea50f525ae408923c72
Leon Feldberg <lfeldberg@hotmail.com>
2024-07-05 21:13:38 -0400

    corrected app.run() parameters

--- web/app.py
+++ web/app.py
@@ -284,4 +284,4 @@ api.add_resource(TakeLoan, '/take_loan')
 api.add_resource(PayLoan, '/pay_loan')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
